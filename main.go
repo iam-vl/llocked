196f06e7eac97b51ba19b13bad5cc54131b32c8a
vl <gztrk@engineer.com>
2024-05-29 15:14:32 +0400

    complete ch6.1 - add views

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/iam-vl/llocked/views"
 )
 
 func main() {
@@ -60,16 +60,18 @@ func HandleGallery(w http.ResponseWriter, r *http.Request) {
 func ExecuteTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	tpl, err := template.ParseFiles(filepath)
+	tpl, err := views.Parse(filepath)
+	// tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "Error parsing the template", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "Error executing the template", http.StatusInternalServerError)
-		return
-	}
+	// err = tpl.Execute(w, nil)
+	// if err != nil {
+	// 	log.Printf("executing template: %v", err)
+	// 	http.Error(w, "Error executing the template", http.StatusInternalServerError)
+	// 	return
+	// }
+	tpl.Execute(w, nil)
 }
